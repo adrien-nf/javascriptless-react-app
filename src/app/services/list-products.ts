e357fc7faa694c7a866dd68723046bac09eb5a99
Adrien N. F <iamnautilusjungle@gmail.com>
2025-11-16 12:04:05 +0100

    feat: update to list more products on page

--- src/app/services/list-products.ts
+++ src/app/services/list-products.ts
@@ -2,5 +2,5 @@ import { Product } from "@/types";
 import data from "@/data/products.json";
 
 export async function listProducts(): Promise<Product[]> {
-  return data.products.slice(0, 8);
+  return data.products.slice(0, 80);
 }
